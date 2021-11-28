a88b9b77eb368b363fdd2080a0165bbe31746d5d
Agnieszka Witek <witek.aga@gmail.com>
2021-11-28 20:18:31 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div>
+  <h1>Cześć, jak masz na imię?</h1>
+    <input type="text" placeholder="Twoje imię" /> 
+    <button>Dalej</button>
+    </div>
+  );
+}
+
+export default App;
